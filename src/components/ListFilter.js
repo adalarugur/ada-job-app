d16a4ca53233b729df7da8d9c4282dd5aab77797
adalarugur <adalarugur@gmail.com>
2022-06-14 01:54:33 +0300

    commit3

--- src/components/ListFilter.js
+++ src/components/ListFilter.js
@@ -2,27 +2,39 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Form, Row, Col } from "react-bootstrap";
-import { updateFilterStatus } from "../slices/jobSlice";
+import { updateFilterStatus, updateFilterText } from "../slices/jobSlice";
 
 function ListFilter() {
   const initialFilterStatus = useSelector((state) => state.job.filterStatus);
   const [filterStatus, setFilterStatus] = useState(initialFilterStatus);
+
+  const initialFilterText = useSelector((state) => state.job.filterText);
+  const [filterText, setFilterText] = useState(initialFilterText);
+
   const dispatch = useDispatch();
 
-  const updateFilter = (e) => {
-    setFilterStatus(e.target.value);
-    dispatch(updateFilterStatus(e.target.value));
+  const updateFilter = (e,type) => {
+      debugger;
+    
+    if(type==="selectType"){
+        setFilterStatus(e.target.value);
+        dispatch(updateFilterStatus(e.target.value));
+    }else if(type==="inputType"){
+        setFilterText(e.target.value);
+        dispatch(updateFilterText(e.target.value));
+    }
+    
   };
   return (
     <Form >
       <Row>
         <Col>
-          <input type="text" placeholder="search job"></input>
+          <input type="search" placeholder="search job" onChange={(e,type) => updateFilter(e,"inputType")} value={filterText} ></input>
         </Col>
         <Col>
           <Form.Select
             id="status"
-            onChange={(e) => updateFilter(e)}
+            onChange={(e,type) => updateFilter(e,"selectType")}
             value={filterStatus}
           >
             <option value="all">All</option>
